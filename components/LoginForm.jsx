61e56f3977347fa4b9133be3b28784a344ace418
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-06-13 08:01:49 +0200

    Login with credentials and google provider added

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -4,9 +4,15 @@ import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
 import { signIn, getProviders, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 const LoginForm = () => {
   const [providers, setProviders] = useState(null);
+
+  const router = useRouter();
+
+  const { data: session, status: sessionStatus } = useSession();
 
   useEffect(() => {
     const setAuthProviders = async () => {
@@ -17,19 +23,61 @@ const LoginForm = () => {
     setAuthProviders();
   }, []);
 
+  useEffect(() => {
+    if (sessionStatus === "authenticated") {
+      router.replace("/");
+    }
+  }, [sessionStatus, router]);
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    if (!isValidEmail(email)) {
+      toast.error("Email is invalid");
+      return;
+    }
+
+    if (!password || password.length < 8) {
+      toast.error("Password is invalid");
+      return;
+    }
+
+    const res = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+
+    if (res?.error) {
+      toast.error("Invalid email or password");
+      if (res?.url) router.replace("/");
+    }
+  };
+
+  if (sessionStatus === "loading") {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div className="flex h-fit flex-col items-center gap-3 tracking-wide text-neutral-300">
       <h1 className="text-xl font-bold underline underline-offset-8">Login</h1>
       <div className="m-auto w-full max-w-xs rounded-lg border border-solid border-neutral-800 bg-neutral-950 p-5">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            <label className="mb-2 block" htmlFor="username">
-              Username or Email
+            <label className="mb-2 block" htmlFor="email">
+              Email
             </label>
             <input
               className="red-indigo-700 mb-6 w-full rounded-md border-b-2 border-neutral-300 p-2 text-neutral-950 outline-none focus:bg-neutral-500"
               type="text"
-              id="username"
+              id="email"
               required
             />
           </div>
@@ -53,23 +101,26 @@ const LoginForm = () => {
         </form>
 
         {providers &&
-          Object.values(providers).map((provider, index) => (
-            <button
-              key={index}
-              onClick={() =>
-                signIn(provider.id, {
-                  callbackUrl: "/",
-                })
-              }
-              className="mb-6 flex w-full justify-center rounded border-neutral-300 bg-slate-500 px-4 py-2 font-bold transition-all duration-300 hover:bg-slate-700 hover:shadow-button hover:shadow-slate-700"
-              type="submit"
-            >
-              <span className="flex items-center gap-3">
-                <FaGoogle />
-                Login with Google
-              </span>
-            </button>
-          ))}
+          Object.values(providers).map(
+            (provider, index) =>
+              index === 1 && (
+                <button
+                  key={index}
+                  onClick={() =>
+                    signIn(provider.id, {
+                      callbackUrl: "/",
+                    })
+                  }
+                  className="mb-6 flex w-full justify-center rounded border-neutral-300 bg-slate-500 px-4 py-2 font-bold transition-all duration-300 hover:bg-slate-700 hover:shadow-button hover:shadow-slate-700"
+                  type="submit"
+                >
+                  <span className="flex items-center gap-3">
+                    <FaGoogle />
+                    Login with Google
+                  </span>
+                </button>
+              ),
+          )}
 
         <div>
           <div className="flex justify-between gap-4">
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,10 +1,37 @@
 import connectDB from "@/config/database";
 import User from "@/models/User";
-
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import bcrypt from "bcryptjs";
 
 export const authOptions = {
   providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await connectDB();
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password,
+            );
+            if (isPasswordCorrect) {
+              return user;
+            }
+          }
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    }),
+
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -19,25 +46,29 @@ export const authOptions = {
   ],
   callbacks: {
     // Invoked on successful signin
-    async signIn({ profile }) {
-      // console.log(account.provider);
-
-      // 1. Connect to database
-      await connectDB();
-      // 2. Check if user exists
-      const userExists = await User.findOne({ email: profile.email });
-      // 3. If not, then add user to database
-      if (!userExists) {
-        // Truncate user name if too long
-        const username = profile.name.slice(0, 20).toLowerCase();
-
-        await User.create({
-          username,
-          email: profile.email,
-        });
+    async signIn({ profile, account }) {
+      if (account?.provider == "credentials") {
+        return true;
       }
-      // 4. Return true to allow sign in
-      return true;
+
+      if (account?.provider === "google") {
+        // 1. Connect to database
+        await connectDB();
+        // 2. Check if user exists
+        const userExists = await User.findOne({ email: profile.email });
+        // 3. If not, then add user to database
+        if (!userExists) {
+          // Truncate user name if too long
+          const username = profile.name.slice(0, 20).toLowerCase();
+
+          await User.create({
+            username,
+            email: profile.email,
+          });
+        }
+        // 4. Return true to allow sign in
+        return true;
+      }
     },
     // Modifies the session object
     async session({ session }) {
