0a83ad6b8362dae56ec09954f532832ed60e3fd2
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-06-26 11:31:25 +0200

    Capitalize letter

--- components/PopularSportsLink.jsx
+++ components/PopularSportsLink.jsx
@@ -11,7 +11,7 @@ const PopularSportsLink = ({ competition }) => {
       <span
         className={`absolute bottom-0 left-0 h-0.5 w-0 bg-red-500 duration-300 group-hover:w-full group-hover:transition-all ${pathname.includes(competition) ? "w-full" : ""}`}
       ></span>
-      {competition}
+      {competition.charAt(0).toUpperCase() + competition.slice(1)}
     </Link>
   );
 };
