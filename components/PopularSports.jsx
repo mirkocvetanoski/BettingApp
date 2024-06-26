91d831372c20360cf6e01562b5f025c4440219d7
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-06-26 12:54:31 +0200

    Fixing deploying errors

--- components/PopularSports.jsx
+++ components/PopularSports.jsx
@@ -7,11 +7,13 @@ const PopularSports = async () => {
   const sortedCompetitions = competitions?.sort((a, b) => a.localeCompare(b));
 
   return (
-    <ul className="grid h-fit auto-cols-max grid-flow-col grid-rows-3 items-center justify-items-center gap-x-1 gap-y-0.5 rounded-lg border border-solid border-neutral-800 px-5 py-4">
-      {sortedCompetitions.map((competition, index) => (
-        <PopularSportsLink key={index} competition={competition} />
-      ))}
-    </ul>
+    competitions.length > 0 && (
+      <ul className="grid h-fit auto-cols-max grid-flow-col grid-rows-3 items-center justify-items-center gap-x-1 gap-y-0.5 rounded-lg border border-solid border-neutral-800 px-5 py-4">
+        {sortedCompetitions.map((competition, index) => (
+          <PopularSportsLink key={index} competition={competition} />
+        ))}
+      </ul>
+    )
   );
 };
 
