d9055cf847b2273d8c1574d857c4aac7e720b07e
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-06-26 13:25:45 +0200

    Fixing deploying errors

--- utils/requests.js
+++ utils/requests.js
@@ -8,7 +8,10 @@ async function fetchCompetitions() {
       return [];
     }
 
-    const res = await fetch(`${apiDomain}/competitions`, { cache: "no-store" });
+    const res = await fetch(
+      `${apiDomain}/competitions`,
+      // { cache: "no-store" }
+    );
 
     if (!res.ok) {
       throw new Error("Failed to fetch data");
