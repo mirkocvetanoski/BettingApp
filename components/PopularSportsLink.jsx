79a0aa654d7822a235cde0338a1efff5ebc60667
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-06-26 14:19:16 +0200

    Trimming strings from pathname

--- components/PopularSportsLink.jsx
+++ components/PopularSportsLink.jsx
@@ -9,7 +9,7 @@ const PopularSportsLink = ({ competition }) => {
   return (
     <Link href={`/${competition}`} className="group relative px-2.5 py-1.5">
       <span
-        className={`absolute bottom-0 left-0 h-0.5 w-0 bg-red-500 duration-300 group-hover:w-full group-hover:transition-all ${pathname === "/" && competition === "Football" ? "w-full" : ""} ${pathname.includes(competition.replace(/[^A-Za-z]+/g, "")) ? "w-full" : ""}`}
+        className={`absolute bottom-0 left-0 h-0.5 w-0 bg-red-500 duration-300 group-hover:w-full group-hover:transition-all ${pathname === "" && competition === "Football" ? "w-full" : ""} ${pathname.includes(competition.replace(/[^A-Za-z]+/g, "")) ? "w-full" : ""}`}
       ></span>
       {competition.charAt(0).toUpperCase() + competition.slice(1)}
     </Link>
