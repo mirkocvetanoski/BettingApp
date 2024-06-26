d5588f9461717285b64b4afe8dbac173dbaa3e5a
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-06-26 11:25:27 +0200

    Popular Sports fetched

--- components/PopularSportsLink.jsx
+++ components/PopularSportsLink.jsx
@@ -1,12 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const PopularSportsLink = ({ competition }) => {
+  const pathname = usePathname();
+
   return (
-    <Link
-      href={`/competitions/${competition}`}
-      className="group relative px-2.5 py-1.5"
-    >
-      <span className="absolute bottom-0 left-0 h-0.5 w-0 bg-red-500 duration-300 group-hover:w-full group-hover:transition-all"></span>
+    <Link href={`/${competition}`} className="group relative px-2.5 py-1.5">
+      <span
+        className={`absolute bottom-0 left-0 h-0.5 w-0 bg-red-500 duration-300 group-hover:w-full group-hover:transition-all ${pathname.includes(competition) ? "w-full" : ""}`}
+      ></span>
       {competition}
     </Link>
   );
