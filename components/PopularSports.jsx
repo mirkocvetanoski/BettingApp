ea4c0c44faa1641b7429dbdec5dff32da2ceb944
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-06-27 07:44:42 +0200

    Solving deploying problem

--- components/PopularSports.jsx
+++ components/PopularSports.jsx
@@ -1,5 +1,4 @@
 import { fetchCompetitions } from "@/utils/requests";
-import Link from "next/link";
 import PopularSportsLink from "./PopularSportsLink";
 
 const PopularSports = async () => {
@@ -10,14 +9,7 @@ const PopularSports = async () => {
   return (
     <ul className="grid h-fit auto-cols-max grid-flow-col grid-rows-3 items-center justify-items-center gap-x-1 gap-y-0.5 rounded-lg border border-solid border-neutral-800 px-5 py-4">
       {sortedCompetitions.map((competition, index) => (
-        // <PopularSportsLink key={index} competition={competition} />
-        <Link
-          href={`/${competition}`}
-          className="group relative px-2.5 py-1.5"
-          key={index}
-        >
-          {competition}
-        </Link>
+        <PopularSportsLink key={index} competition={competition} />
       ))}
     </ul>
   );
