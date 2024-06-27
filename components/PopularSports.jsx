42fa7ac32c9a8a095d52841cc8cdc13b0da66f59
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-06-27 07:50:26 +0200

    Solving deploying problem

--- components/PopularSports.jsx
+++ components/PopularSports.jsx
@@ -4,11 +4,11 @@ import PopularSportsLink from "./PopularSportsLink";
 const PopularSports = async () => {
   const { competitions } = await fetchCompetitions();
 
-  const sortedCompetitions = competitions?.sort((a, b) => a.localeCompare(b));
+  // const sortedCompetitions = competitions?.sort((a, b) => a.localeCompare(b));
 
   return (
     <ul className="grid h-fit auto-cols-max grid-flow-col grid-rows-3 items-center justify-items-center gap-x-1 gap-y-0.5 rounded-lg border border-solid border-neutral-800 px-5 py-4">
-      {sortedCompetitions.map((competition, index) => (
+      {competitions.map((competition, index) => (
         <PopularSportsLink key={index} competition={competition} />
       ))}
     </ul>
