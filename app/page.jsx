f66501582cf6a4da44023358ac92c414af4e76b8
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-07-09 08:35:18 +0200

    MatchesList component added and calendar implemented from primereact

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,15 @@
 import CountriesList from "@/components/CountriesList";
+import MatchesList from "@/components/MatchesList";
 import PopularSports from "@/components/PopularSports";
 
 export default function Home() {
   return (
     <section className="bg-red flex w-full flex-col justify-center text-sm text-neutral-300">
       <PopularSports />
-      <CountriesList />
+      <div className="flex items-start justify-between">
+        <CountriesList />
+        <MatchesList />
+      </div>
     </section>
   );
 }
---
+++ components/MatchesList.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Calendar } from "primereact/calendar";
+import "primereact/resources/themes/mira/theme.css";
+import { useState } from "react";
+
+const MatchesList = () => {
+  const [date, setDate] = useState(null);
+
+  const TODAY = new Date();
+  const minDate = new Date();
+  const maxDate = new Date();
+  minDate.setDate(TODAY.getDate() - 5);
+  maxDate.setDate(TODAY.getDate() + 5);
+
+  return (
+    <div className="relative mt-8 flex h-[550px] w-3/4 flex-col items-start justify-start rounded-lg border border-solid border-neutral-800 px-5 py-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <button>ALL</button>
+          <button>LIVE</button>
+          <button>ODDS</button>
+          <button>FINISHED</button>
+          <button>SCHEDULED</button>
+        </div>
+      </div>
+      <Calendar
+        value={date}
+        onChange={(e) => setDate(e.value)}
+        dateFormat="dd/mm/yy"
+        showIcon
+        minDate={minDate}
+        maxDate={maxDate}
+        className="absolute right-4 top-4"
+      />
+
+      <div>Matches</div>
+    </div>
+  );
+};
+
+export default MatchesList;
