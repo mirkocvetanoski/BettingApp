fdfe8504b776d6f8cd31069823ad9453860f8c8d
MirkoCvetanoski <mirko.cvetanoski@gmail.com>
2024-07-09 07:28:10 +0200

    Arrow icon added

--- components/CountriesListLink.jsx
+++ components/CountriesListLink.jsx
@@ -4,6 +4,7 @@ import { fetchLeagues } from "@/utils/requests";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import LeaguesList from "@/components/LeaguesList";
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 
 const CountriesListLink = ({ country }) => {
   const pathname = usePathname().replace(/[^A-Za-z]+/g, "");
@@ -21,12 +22,15 @@ const CountriesListLink = ({ country }) => {
     <>
       <button
         onClick={handleOnClick}
-        className="group relative w-full px-2.5 py-1.5 text-start"
+        className="group relative flex w-full items-center justify-between px-2.5 py-1.5 text-start"
       >
         <span
           className={`absolute bottom-0 left-0 h-0.5 w-0 bg-red-500 duration-300 group-hover:w-full group-hover:transition-all ${pathname.includes(country.replace(/[^A-Za-z]+/g, "")) ? "w-full" : ""}`}
         ></span>
         {country}
+        <span className="text-[8px]">
+          {isOpen ? <SlArrowUp /> : <SlArrowDown />}
+        </span>
       </button>
       {isOpen && <LeaguesList leagues={leagues} />}
     </>
